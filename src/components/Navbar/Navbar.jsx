2d31613f9b42398a2e861b1bead29b75d9fad76f
AustinMusuya <amusuya57@gmail.com>
2025-01-26 16:14:04 +0300

    create components Hero, Navbar, Services

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+// import React from 'react'
+import Logo from "../../assets/logo.png"
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 right-0 w-full z-50 bg-black/10 backdrop-blur-sm py-4 sm:py-4">
+        <div className='container'>
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center gap-4 text-whute font-bold text-2xl'>
+                    <img src={Logo} alt="" className="w-20" />
+                    <span className="text-white">Marine Aquatics Kenya</span>
+                </div>
+                <div className="text-white">
+                    <ul className="flex items-center gap-6 text-xl py-4 sm:py-0">
+                        <li>
+                            <a href="">About</a>
+                        </li>
+                        <li>
+                            <a href="">Tec</a>
+                        </li>
+                        <li>
+                            <a href="">Something</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <button className="text-white border-2 borger-white px-3 py-1 rounded-md">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
