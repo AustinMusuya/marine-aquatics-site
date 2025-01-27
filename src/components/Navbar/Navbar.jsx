adff10546d09c1d193090a7d2e9fe1b010e588db
AustinMusuya <amusuya57@gmail.com>
2025-01-27 09:31:04 +0300

    add aos for navbar animation on page load and scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import Logo from "../../assets/logo.png"
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 right-0 w-full z-50 bg-black/10 backdrop-blur-sm py-4 sm:py-4">
+    <div data-aos="fade-down" className="fixed top-0 right-0 w-full z-50 bg-black/10 backdrop-blur-sm py-4 sm:py-4">
         <div className='container'>
             <div className='flex justify-between items-center'>
                 <div className='flex items-center gap-4 text-whute font-bold text-2xl'>
@@ -13,13 +13,13 @@ const Navbar = () => {
                 <div className="text-white">
                     <ul className="flex items-center gap-6 text-xl py-4 sm:py-0">
                         <li>
-                            <a href="">About</a>
+                            <a href="">About Us</a>
                         </li>
                         <li>
-                            <a href="">Tec</a>
+                            <a href="">Services</a>
                         </li>
                         <li>
-                            <a href="">Something</a>
+                            <a href="">Contact Us</a>
                         </li>
                     </ul>
                 </div>
