d8b36ffa62e6106afd0ad6e4f4450feb38dfd16f
austinmusuya <amusuya57@gmail.com>
2025-01-28 04:13:14 +0300

    format code with prettier

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,17 +8,27 @@ const Hero = () => {
         <div className="container grid grid-cols-1 sm:grid-cols-2 gap-4">
           {/* Text Content */}
           <div className="space-y-4 lg:pr-36">
-            <h1 data-aos="fade-up" className="text-3xl sm:text-4xl md:text-5xl font-bold">
+            <h1
+              data-aos="fade-up"
+              className="text-3xl sm:text-4xl md:text-5xl font-bold"
+            >
               Explore the World of Live Ornamental Fish
             </h1>
-            <p data-aos="fade-up" data-aos-delay="300" className="text-sm sm:text-base md:text-lg">
-              Discover vibrant, exotic fish species shipped with care and precision. We ensure safe, reliable delivery with unbeatable rates and IATA-approved handling.
+            <p
+              data-aos="fade-up"
+              data-aos-delay="300"
+              className="text-sm sm:text-base md:text-lg"
+            >
+              Discover vibrant, exotic fish species shipped with care and
+              precision. We ensure safe, reliable delivery with unbeatable rates
+              and IATA-approved handling.
             </p>
 
             <button
               data-aos="fade-up"
               data-aos-delay="300"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-transform hover:scale-105 cursor-pointer">
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-transform hover:scale-105 cursor-pointer"
+            >
               Learn More
             </button>
           </div>
@@ -29,12 +39,11 @@ const Hero = () => {
       <img
         src={MountainPng}
         alt="Mountain background"
-        className="absolute right-0 bottom-0 w-full brightness-50 z-10 object-cover" />
+        className="absolute right-0 bottom-0 w-full brightness-50 z-10 object-cover"
+      />
 
       {/* Gradient Bottom Section */}
-      <div
-        className="absolute bottom-0 w-full z-30 bg-gradient-to-b from-transparent from-10% to-black to-90% h-[20px] sm:h-[50px] md:h-[60px]">
-      </div>
+      <div className="absolute bottom-0 w-full z-30 bg-gradient-to-b from-transparent from-10% to-black to-90% h-[20px] sm:h-[50px] md:h-[60px]"></div>
     </div>
   );
 };
